50b36dc398d6973488e0685204fd491224ea6398
Ian Irving <ian@falsepositives.com>
2024-03-01 17:21:38 -0500

    Add getJsonData function to retrieve JSON data from a file

---
+++ src/getJsonData.js
@@ -0,0 +1,18 @@
+import path from "path";
+import fs from "fs-extra";
+
+export const getJsonData = async (subDirectory, fileName) => {
+  // console.log("subDirectory", subDirectory);
+  // console.log("fileName", fileName);
+  // get external data json
+  const dataFilePath = path.join(
+    process.cwd(),
+    subDirectory,
+    `${fileName}.json`
+  );
+  console.log("dataFilePath", dataFilePath);
+  const dataFile = await fs.readFile(dataFilePath, "utf-8");
+  const data = JSON.parse(dataFile);
+  // console.log("data", data);
+  return data;
+};
