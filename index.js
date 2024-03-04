90c68997029c8a06c910cff01091a56193759b06
Ian Irving <ian@falsepositives.com>
2024-03-04 11:29:16 -0500

    Refactor code to use async/await syntax for generating PDF

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-// import { pdfGenerator } from "./src/pdfGenerator.js";
 import generatePdf from "./src/generatePdf.js";
 
-// async function generatePdf() {
-const fileName = "Ians test file";
-const dataFileName = "sampleData";
-let buffer = await generatePdf(fileName, dataFileName);
-
-//you can use this buffer if you want to upload this file to any storage (like: AWS S3)
-// }
-
-// generatePdf();
+try {
+  (async () => {
+    const fileName = "Ians test file";
+    const dataFileName = "sampleData";
+    let buffer = await generatePdf(fileName, dataFileName);
+    //you can use this buffer if you want to upload this file to any storage (like: AWS S3)
+    console.log("done");
+  })();
+} catch (error) {
+  console.error(error);
+}
